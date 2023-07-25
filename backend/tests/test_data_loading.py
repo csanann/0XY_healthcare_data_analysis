208c85ff5a6120c2183913b2c5c61f617f1ffd23
csanann <csanann@gmail.com>
2023-07-25 22:13:40 +0100

    Updated code to read data file from the correct path

--- backend/tests/test_data_loading.py
+++ backend/tests/test_data_loading.py
@@ -1,25 +1,24 @@
 # file: 0XY_healthcare_data_analysis/backend/tests/test_data_loading.py
 
 import sys
-import os
 from pathlib import Path
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.dirname(current_directory))
+current_file = Path(__file__)
+test_dir = current_file.parent
+project_root = test_dir.parent
+sys.path.insert(0, str(project_root))
 
 import pandas as pd
 from data_loading import load_data
- 
+
 def test_load_data():
-  print("Current working directory:", os.getcwd())
-  print("Contents of the 'data' directory:", os.listdir('data'))
-  
-  if sys.platform.startswith("win"):
-    data_file_path = Path('data/data-ori.csv')
-  else:
-    data_file_path = Path('data/data-ori.csv')
-    
-  df = load_data(data_file_path)
-  assert isinstance(df, pd.DataFrame)
-  assert df.shape[0] > 0
-  assert df.shape[1] > 0
+    print("Current working directory:", project_root)
+
+    data_dir = project_root / 'data'
+    print("Contents of the 'data' directory:", list(data_dir.glob('*')))
+
+    data_file_path = data_dir / 'data-ori.csv'
+    df = load_data(data_file_path)
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape[0] > 0
+    assert df.shape[1] > 0
