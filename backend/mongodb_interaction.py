2206b2579e5f4a6b8167de7f6806154393bde13c
csanann <csanann@gmail.com>
2023-08-11 15:24:36 +0000

    Updates the connection to localhost

--- backend/mongodb_interaction.py
+++ backend/mongodb_interaction.py
@@ -7,35 +7,25 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env")
 
-MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
-MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
-MONGODB_URI = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster.mongodb.net/test?retryWrites=true&w=majority"
-
+MONGODB_URI = 'mongodb://locahost:27017/'
 client = MongoClient(MONGODB_URI)
 db = client['heath_records_db']
 
 
 def load_data_to_db(data_filepath):
-  client = MongoClient('mongodb://localhost:27017/')
-  db = client['health_records_db']
-  
   df = pd.read_csv(data_filepath)
   df.reset_index(inplace = True)
   df.rename(columns = {'index': 'patient_id'}, inplace = True)
   
   records = df.to_dict(orient = 'records')
   db.health_records.insert_many(records)
-  print("Data loaded into MongoDB successfully and you are happy!")
+  print("Data loaded into MongoDB successfully!")
   
 def get_all_records():
-  client = MongoClient('mongodb://localhost:27017/')
-  db = client['health_records_db']
   records = db.health_records.find()
   return records
   
 def get_record_by_id(id):
-  client = MongoClient('mongodb://localhost:27017/')
-  db = client['health_records_db']
   record = db.health_records.find_one({'patient_id': id})
   return record
   
