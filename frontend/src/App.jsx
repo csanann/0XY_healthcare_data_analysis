10436f48d49a527e548154b08d9b3f1e02fdf64f
csanann <csanann@gmail.com>
2023-08-11 15:03:44 +0000

    Updates code to suits the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,35 @@
 // /0XY_healthcare_data_analysis/frontend/src/App.jsx
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import DataFetcher from './DataFetcher.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0); // consider for later use
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <DataFetcher/>
+      <div id='body' className="flex-column min-100-vh">
+        <header className='hero'>
+          <h1 className='app-title'>Healthcare Analysis Tool</h1>
+          <p>Analyse and visualise healthcare data to gain insights.</p>
+        </header>
+        <main className='flex-row justify-space-between'>
+          <div className="card">
+          <button onClick={() => {/* load or analyse data here */}}>
+            Load Latest Data
+          </button>
+          <p>
+          Select parameters fro the options above/drop down and click "Load Latest Data" to begin analysis.
+          </p>
+        </div>
+        {/* Adding visual elements Charts/tables here */}
+        
+        <DataFetcher/>
+      </main>
+    </div>
     </>
   );
 }
 
-export default App
+export default App;
