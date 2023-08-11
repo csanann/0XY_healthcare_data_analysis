3a9411a654b9f9c4ed449cfccfc795c94cc86110
csanann <csanann@gmail.com>
2023-08-11 16:35:48 +0000

    Refactors and tidy up codebase

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,45 +1,43 @@
 # file: /0XY_healthcare_data_analysis/backend/__init__.py
 
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from mongodb_interaction import get_all_records, get_record_by_id
-from eda import load_data, analyse
+from pymongo import MongoClient
+from backend.mongodb_interaction import load_data, analyse, get_all_records, get_record_by_id
 import os
 
-app = Flask(__name__, static_folder='../public')
-CORS(app)
+# Connect to MongoDB
+MONGODB_URI = 'mongodb://localhost:27017/'
+client = MongoClient(MONGODB_URI)
+db = client['health_records_db']
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
+    @app.route("/", methods=['GET'])
+    def home():
+        return app.send_static_file('index.html')
 
-@app.route('/records', methods = ['GET'])
-def records():
-    records = get_all_records()
-    return jsonify(records)
-  
-@app.route('/record/<id>', methods = ['GET'])  
-def record(id):
-    record = get_record_by_id(id)
-    return jsonify(record)
-    
-@app.route('/analyse', methods = ['GET'])
-def analyse_endpoint():
-    df = load_data('data/data-ori.csv')
-    
-    variable = request.args.get('variable')
-    
-    if variable not in [ 'HAEMATOCRIT', 'HAEMOGLOBINS', 'ERYTHROCYTE', 'LEUCOCYTE', 'THROMBOCYTE', 'MCH', 'MCHE', 'MCV', 'AGE', 'SEX', 'SOURCE']:
-        return jsonify({'error': 'Invalid variable'}), 400
+    @app.route('/records', methods=['GET'])
+    def records():
+        records = get_all_records()
+        return jsonify(records)
+
+    @app.route('/record/<id>', methods=['GET'])
+    def record(id):
+        record = get_record_by_id(id)
+        return jsonify(record)
+
+    @app.route('/analyse', methods=['GET'])
+    def analyse_endpoint():
+        df = load_data('data/data-ori.csv')
+        variable = request.args.get('variable')
         
-    df = analyse(df, variable)
-    return df.to_json(orient = 'records')
-    
-@app.route('/create')
-def home():
-    return app.send_static_file('index.html')
+        if variable not in ['HAEMATOCRIT', 'HAEMOGLOBINS', 'ERYTHROCYTE', 'LEUCOCYTE', 'THROMBOCYTE', 'MCH', 'MCHE', 'MCV', 'AGE', 'SEX', 'SOURCE']:
+            return jsonify({'error': 'Invalid variable'}), 400
+            
+        df = analyse(df, variable)
+        return df.to_json(orient='records')
+
+    return app
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # file: /0XY_healthcare_data_analysis/main.py
 
-from backend import app
+from backend import create_app
 import os
+
+app = create_app()
 
 port = int(os.environ.get("PORT", 5000))
 
 if __name__=='__main__':
-    app.run(host = '0.0.0.0', port = port, debug = False)
+    app.run(host='0.0.0.0', port=port, debug = False)
