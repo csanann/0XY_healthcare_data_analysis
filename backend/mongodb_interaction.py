95b6e503a646051875af84a1431f9e1d1802f846
csanann <csanann@gmail.com>
2023-08-11 16:31:07 +0000

    Updates code and correct typo

--- backend/mongodb_interaction.py
+++ backend/mongodb_interaction.py
@@ -3,14 +3,19 @@
 from pymongo import MongoClient
 import pandas as pd
 import os
+from eda import load_data
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env")
 
-MONGODB_URI = 'mongodb://locahost:27017/'
+MONGODB_URI = 'mongodb://localhost:27017/'
 client = MongoClient(MONGODB_URI)
-db = client['heath_records_db']
-
+db = client['health_records_db']
+  
+def analyse(df, variable):
+  #placeholder
+  df = df.groupby(variable).count()
+  return df
 
 def load_data_to_db(data_filepath):
   df = pd.read_csv(data_filepath)
@@ -26,8 +31,8 @@ def get_all_records():
   return records
   
 def get_record_by_id(id):
-  record = db.health_records.find_one({'patient_id': id})
+  record = db.health_records.find_one({'patient_id': int(id)})
   return record
   
-if __name__ == "__main__":
+if __name__ == "__main__": # for testing
   load_data_to_db("data/data-ori.csv")
