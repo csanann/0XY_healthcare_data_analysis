46e2af662e34246e795bba534d2d1eae185da2e7
csanann <csanann@gmail.com>
2023-10-06 20:20:14 +0000

    Refoctored test code by adds additional checks on null values Row data and Column names to verify data

--- backend/tests/test_data_loading.py
+++ backend/tests/test_data_loading.py
@@ -1,24 +1,18 @@
 # file: 0XY_healthcare_data_analysis/backend/tests/test_data_loading.py
 
-import sys
-from pathlib import Path
-
-current_file = Path(__file__)
-test_dir = current_file.parent
-project_root = test_dir.parent
-sys.path.insert(0, str(project_root))
-
 import pandas as pd
 from data_loading import load_data
 
 def test_load_data():
-    print("Current working directory:", project_root)
-
-    data_dir = project_root / 'data'
-    print("Contents of the 'data' directory:", list(data_dir.glob('*')))
-
-    data_file_path = data_dir / 'data-ori.csv'
+    data_file_path = 'data/data-ori.csv'
     df = load_data(data_file_path)
+    
     assert isinstance(df, pd.DataFrame)
     assert df.shape[0] > 0
     assert df.shape[1] > 0
+    
+    expect_cols = ['HAEMATOCRIT', 'HAEMOGLOBINS', 'ERYTHROCYTE', 'LEUCOCYTE']
+    assert set(expect_cols).issubset(set(df.columns))
+    assert df.loc[0, 'HAEMATOCRIT'] == 321
+    assert df.loc[100, 'ERYTHROCYTE'] == 4.41
+    assert df.isnull().sum().sum() == 0
