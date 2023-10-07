e23bbb1ac548c7c238a6bac9671a210311e2b778
csanann <csanann@gmail.com>
2023-10-07 17:13:20 +0000

    Refactored dependencies to fix the error in ci-cd pipeline

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'matplotlib',
-        'seaborn',
-        'scikit-learn'
+        'pandas>=1.0.0',
+        'matplotlib>=3.0.0',
+        'scikit-learn>1.0.0',
+        'Flask>1.1.4',
+        'Werkzeug>=2.0.0,<2.1.0'
     ]
 )
