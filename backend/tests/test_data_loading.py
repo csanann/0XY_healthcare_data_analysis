6fca0321e16da806f83407e646b026e250ba5d5b
csanann <csanann@gmail.com>
2023-10-10 20:50:43 +0000

    Refactored file path

--- backend/tests/test_data_loading.py
+++ backend/tests/test_data_loading.py
@@ -8,19 +8,27 @@ import os
 def test_load_data():
     data_file_path = os.path.join(os.path.dirname(__file__), '../data/data-ori.csv')
     with open(data_file_path, 'r') as f:
-        df = load_data(f)
+        df_train, df_val, df_test = load_data(f)
     
     #check bug
-    print(df.head())
-    print(df.info())
+    print("Train DataFrame:")
+    print(df_train.head())
+    print("Validation DataFrame:")
+    print(df_val.head())
+    print("Test DataFrame:")
+    print(df_test.head())
     
-    assert isinstance(df, pd.DataFrame)
+    assert isinstance(df_train, pd.DataFrame)
+    assert isinstance(df_val, pd.DataFrame)
+    assert isinstance(df_test, pd.DataFrame)
     
-    assert df.shape[0] > 0
-    assert df.shape[1] > 0
+    assert df_train.shape[0] > 0
+    assert df_val.shape[0] > 0
+    assert df_test.shape[0] > 0
     
     expect_cols = ['HAEMATOCRIT', 'HAEMOGLOBINS', 'ERYTHROCYTE', 'LEUCOCYTE']
-    assert set(expect_cols).issubset(set(df.columns))
-    assert df.loc[0, 'HAEMATOCRIT'] == 321
-    assert df.loc[100, 'ERYTHROCYTE'] == 4.41
-    assert df.isnull().sum().sum() == 0
+    assert set(expect_cols).issubset(set(df_train.columns))
+
+    assert df_train.isnull().sum().sum() == 0
+    assert df_val.isnull().sum().sum() == 0
+    assert df_test.isnull().sum().sum() == 0
