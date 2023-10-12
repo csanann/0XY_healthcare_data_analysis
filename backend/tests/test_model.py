89474656c9fab56b55973e9f275eba6379ab4221
csanann <csanann@gmail.com>
2023-10-12 12:22:44 +0000

    Correct and improve test coverage to pass cicd pipeline error

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -12,8 +12,11 @@ y_train_sample = pd.Series([0.5, 0.7, 0.9, 1.1, 1.3])
 X_val_sample = pd.DataFrame({'feature1': [6, 7, 8, 9, 10], 'feature2': [0.6, 0.7, 0.8, 0.9, 1.0]})
 y_val_sample = pd.Series([1.5, 1.7, 1.9, 2.1, 2.3])
 
-def test_train_model():
-    model, r2, mse = train_model(X_train_sample, y_train_sample, X_val_sample, y_val_sample)
+def test_train_model(tmp_path):
+    data_file = tmp_path / "test_data.csv"
+    X_train_sample.to_csv(data_file, index = False)
+    
+    model, r2, mse = train_model(data_file)
     
     assert isinstance(model, LinearRegression)
     assert 0 <= r2 <= 1 #is r2 in the accepatable range?
@@ -27,7 +30,10 @@ def test_predict():
     assert y_pred.shape == y_val_sample.shape
     
 def test_save_and_load_model(tmp_path):
-    model, _, _ = train_model(X_train_sample, y_train_sample, X_val_sample, y_val_sample)
+    data_file = tmp_path / "test_data.csv"
+    X_train_sample.to_csv(data_file, index = False)
+    
+    model, _, _ = train_model(data_file)
     model_path = os.path.join(tmp_path, 'test_model.pkl')
     
     save_model(model, model_path)
