42f20b46ddf3135fb3a68af8d37c93484e959c50
csanann <csanann@gmail.com>
2023-10-13 19:00:08 +0000

    Refactored the code to solve ValueError

--- backend/data_loading.py
+++ backend/data_loading.py
@@ -6,9 +6,16 @@ from sklearn.model_selection import train_test_split
 def load_data(file_buffer):
   df = pd.read_csv(file_buffer)
   
+  #check bug
+  if len(df) <= 1:
+    raise ValueError("Need more than 1 sample to split data.")
+  
   #split data into typical train/val/test
   df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
   df_val, df_test = train_test_split(df_test, test_size=0.5, random_state=42)
   
-  return df_train, df_val, df_test
+  X_train, y_train = df_train.drop(colums=['target']), df_train['target']
+  X_val, y_val = df_val.drop(columns=['target']), df_val['target']
+  
+  return X_train, X_val, y_train, y_val
   
